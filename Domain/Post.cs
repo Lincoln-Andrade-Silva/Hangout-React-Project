ad6b97eb0f4be18dc987871c6f1f84aabc8f99f2
Lincoln Andrade Silva <lincolnandradesilva@outlook.com>
2022-12-29 10:21:55 -0300

    Create Post entity

---
+++ Domain/Post.cs
@@ -0,0 +1,13 @@
+namespace Domain
+{
+    public class Post
+    {
+        public Guid Id {get; set;}
+        public String Title {get; set;}
+        public DateTime Date {get; set;}
+        public String Description {get; set;}
+        public String City {get; set;}
+        public String Venue {get; set;}
+        public String Category {get; set;}
+    }
+}
